f38c086c6c3a71968e67916a15fe380d9f78e021
ttolaaa <somtolaaa@gmail.com>
2023-06-02 00:34:13 +0900

    rmv Router tag add HashRouter to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <App />
