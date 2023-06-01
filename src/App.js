b5622724199902a2c65836d646ec091e33f1241a
ttolaaa <somtolaaa@gmail.com>
2023-06-02 01:16:35 +0900

    modified render app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setActiveNavItem(navItem);
   }
   return (
-
+    <Router>
     <div>
       <nav className="nav">
         <div>
@@ -87,7 +87,7 @@ function App() {
         <Route path="/about" element={<Aboutme />} />
       </Routes>
     </div>
-
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
-//   <React.StrictMode>
+//   <HashRouter>
 //     <App />
-//   </React.StrictMode>
+//   </HashRouter>
 // );
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
